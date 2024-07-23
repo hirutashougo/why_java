0e97f09b15f124c9e928065efab0f5ba1766b5fe
hirutashougo <ddt.shg.hiruta@gmail.com>
2024-07-23 20:08:08 +0900

    7/23

--- trump/FantanHand.java
+++ trump/FantanHand.java
@@ -1,7 +1,21 @@
 package trump;
 
+/*
+ * クラス名:FantanHand
+ * 概要:七並べ用の手札クラス
+ * 作成者:S.Hiruta
+ * 作成日:2024/07/02
+ */
 public class FantanHand extends Hand{
 	
+	/*
+	 * 関数名：lookCard
+	 * 概要:七並べの手札を見る
+	 * 引数：なし
+	 * 戻り値：なし
+	 * 作成者：S.Hiruta
+	 * 作成日：2024/07/02
+	*/
 	public void lookCard() {
 		
 	}
